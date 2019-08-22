0a966eb41d79b8379e200c4d136a65180f0a9ff7
Hamed <hamed0311@gmail.com>
2019-08-21 22:53:47 -0700

    attack and damage method implemented, rand functions correctly

--- Pokemon/pokemon.cpp
+++ Pokemon/pokemon.cpp
@@ -14,34 +14,44 @@ enum STATUS
 };
 
 
-
 class Pokemon
 {
 
     protected:
 
-    u_int16_t _life;
+        int   _life;
+        bool  _dead;
 
     public:
 
         Pokemon(u_int16_t start_range = 50, u_int16_t end_range = 100)
         {
             this->_life = generate_random(start_range, end_range);
+            this->_dead = false;
         }
 
         virtual ~Pokemon()
         {
         }
 
+        virtual bool isDead() const
+        {
+           return ( this->_life <= 0 ) ? true :  false;
+        }
+
         virtual u_int16_t attack() = 0;
         virtual void damage(u_int16_t) = 0;
 
         // generates random number between numbers provided
-        u_int16_t generate_random(const u_int16_t& start_range,const u_int16_t& end_range) const
+        u_int16_t generate_random(const u_int16_t& start_range, const u_int16_t& end_range) const
         {
             return ( rand() % (end_range - start_range) ) + start_range;
         }
 
+        int get_life() const
+        {
+            return this-> _life;
+        }
 };
 
 class Pikachu : public Pokemon
@@ -58,30 +68,30 @@ class Pikachu : public Pokemon
 
         u_int16_t attack() override
         {
-            u_int8_t tmp = generate_random(1,5);
 
-            if(tmp == DOUBLE)
+            if(generate_random(1,5) == DOUBLE)
+            {
+                std::cout<< "Double Activated" << std::endl;
                 return generate_random(25,40) * 2;
+            }
             else
                 return generate_random(25,40);    
         }
 
         virtual void damage(u_int16_t attacked) override
         {
-            u_int8_t tmp = generate_random(1,4);
-
-            if(tmp == ESCAPE)
-                return;
+        
+            if(generate_random(1,4) == ESCAPE)
+            {
+                std::cout << "Attack Dodged" << std::endl;
+            }
             else
+            {
                 this->_life -= attacked;
-            
+                if(_life < 0)
+                    this->_dead = true;  
+            }
         }
-
-        u_int16_t get_life() const
-        {
-            return this->_life;
-        }
-
 };
 
 
@@ -102,13 +112,15 @@ class Squirtle : public Pokemon
             return generate_random(25,40);
         }
 
-        virtual void damage(u_int16_t attacked) override
+        void damage(u_int16_t attacked) override
         {
             this->_life -= attacked;
+            if(_life < 0)
+                    this->_dead = true;
         }
 };
 
 int main(void)
 {
-
+    srand(time(NULL));
 }
